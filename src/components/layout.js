f3146328d084495fef395a789f1c472d325da0fc
Xocolatl <xocobanan@yahoo.com>
2022-11-12 17:20:32 -0500

    Add imgs, parallax background

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,7 @@ import  "./index.scss"
 
 import Header from "./header"
 
-import BgImage from "../images/bgimg.jpg"
+import BgImage from "../images/bgimg.png"
 
 
 
@@ -42,11 +42,25 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`}/>
       <main>
-        <Container fluid className="bg-secondary" style={{minHeight: "100vh", backgroundImage: `url(${BgImage})`, }}>
+        {/* Parallax scrolling background */}
+        <Container fluid className="bg-secondary" style={{
+          minHeight: "90vh", 
+          width: "100%",
+          backgroundImage: `url(${BgImage})`, 
+          backgroundRepeat: "no-repeat",
+          overflow: "hidden",
+          position: "relative",
+          backgroundAttachment: "fixed",
+          MoxBackgroundSize: "cover",
+          WebkitBackgroundSize: "cover",
+          backgroundSize: "cover",
+          backgroundPosition: "top center",
+          }}>
           <Row className="justify-content-md-center">
             <Col md="auto"
               className="bg-secondary"
-              style={{boxShadow: "0px 0px 30px 40px var(--bs-secondary)", marginTop: "60px"}}
+              // Fuzzy main content box
+              style={{boxShadow: "0px 0px 30px 40px var(--bs-secondary)", marginTop: "50px"}}
               >
               {children}
             </Col>
@@ -56,50 +70,37 @@ const Layout = ({ children }) => {
       <footer>
         <Container fluid
           className="bg-dark mt-auto align-items-center position-absolute text-light"
-          style={{padding:"7px"}}>
-          <Row>
-            <Col style={{fontSize: `0.75rem`}} className="align-self-end">
-              © {new Date().getFullYear()} &middot; Built with
-              {` `}
-              <a href="https://www.gatsbyjs.com">Gatsby</a>
-            </Col>
+          style={{padding:"7px", minHeight: "5vh" }}>
+          <Row className="flex-row-reverse">
             <Col className="text-end" sm={4}>
               <Row className="justify-content-end">
-                <Col className="justify-end">Find me:</Col>
-                <Col className="justify-end">
+                <Col>Find me:</Col>
+                <Col>
                   <Stack gap={1}>
                     <div>
-                    <FaLinkedin/>
-                  <a {...ftLinksProps} href={data.site.siteMetadata?.linkedIn || "/#"}>  LinkedIn</a>
+                      <FaLinkedin/><a {...ftLinksProps} href={data.site.siteMetadata?.linkedIn || "/#"}>  LinkedIn</a>
                     </div>
                     <div>
-                    <FaTwitter/>
-                  <a {...ftLinksProps} href={data.site.siteMetadata?.twitter || "/#"}>  Twitter</a>
+                      <FaTwitter/><a {...ftLinksProps} href={data.site.siteMetadata?.twitter || "/#"}>  Twitter</a>
                     </div>
                   </Stack>
                 </Col>
-                <Col className="justify-end">
+                <Col>
                   <Stack gap={1}>
                     <div>
-                    <FaFlickr/>
-                <a {...ftLinksProps} href={data.site.siteMetadata?.flickr || "/#"}>  Flickr</a>
+                      <FaFlickr/><a {...ftLinksProps} href={data.site.siteMetadata?.flickr || "/#"}>  Flickr</a>
                     </div>
                     <div>
-                    <FaGithub/>
-                <a {...ftLinksProps} href={data.site.siteMetadata?.github || "/#"}>  Github</a>
+                      <FaGithub/><a {...ftLinksProps} href={data.site.siteMetadata?.github || "/#"}>  Github</a>
                     </div>
                   </Stack>
                 </Col>
               </Row>
-                {/* &emsp; <FaLinkedin/>
-                <a {...ftLinksProps} href={data.site.siteMetadata?.linkedIn || "/#"}>  LinkedIn</a>
-                &emsp; <FaTwitter/>
-                <a {...ftLinksProps} href={data.site.siteMetadata?.twitter || "/#"}>  Twitter</a>
-                &emsp; <FaFlickr/>
-                <a {...ftLinksProps} href={data.site.siteMetadata?.flickr || "/#"}>  Flickr</a>
-                &emsp; <FaGithub/>
-                <a {...ftLinksProps} href={data.site.siteMetadata?.github || "/#"}>  Github</a>
-                &emsp; */}
+            </Col>
+            <Col style={{fontSize: `0.75rem`}} className="align-self-end">
+              © {new Date().getFullYear()} &middot; Built with
+              {` `}
+              <a href="https://www.gatsbyjs.com">Gatsby</a>
             </Col>
           </Row>
         </Container>
